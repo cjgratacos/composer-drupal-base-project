f882a392d9294a3ed7297290c66b9bb3e319b18d
Carlos Gratacos <cjgratacos@localhost.localdomain>
2017-07-23 23:08:51 -0400

    feature(Modules): Continue on development

---
+++ modules/entity_test/src/Course/CourseInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\cjgratacos\entity_test\Course;
+
+
+interface CourseInterface
+{
+    function sayHello():string;
+    function hello():string;
+}
---
+++ modules/entity_test/src/Form/CourseSettingsForm.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\cjgratacos\entity_test\Form;
+
+
+class CourseSettingsForm
+{
+
+}
